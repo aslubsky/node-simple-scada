c9d087eca0115c70d966465b8e0bf8a334decfec
Alex Slubsky <aslubsky@gmail.com>
2014-02-23 10:45:39 +0200

    init

--- backend/data-sources/gpio.js
+++ backend/data-sources/gpio.js
@@ -4,13 +4,18 @@ function GPIO(pin, time) {
     this.pin = pin;
     this.time = time;
 
-    this.BIN_PATH = '/';
+    this.BIN_PATH = '/usr/local/bin/gpio';
 
     this.getName = function() {
         return 'gpio'+this.pin;
     }
     
     this.read = function(cb) {
-        cb(null, this, 1);
+        var str = this.BIN_PATH +' read '+this.pin;
+        var self = this;
+        // console.log(__dirname + str);
+        exec(__dirname + str, function(err, stdout, stderr) {
+            cb(err, self, stdout === '1');
+        });
     }
 }
