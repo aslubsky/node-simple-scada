69605a98a2acbd0be9a915f40ea13f4b1e3429c0
Alex Slubsky <aslubsky@gmail.com>
2014-04-11 16:27:54 +0300

    add cog

--- frontend/app/base/canvas/watercounter.js
+++ frontend/app/base/canvas/watercounter.js
@@ -35,6 +35,63 @@ define([
         });
 
 
+        this.draws.push(function () {//cog
+            this.ctx.save();
+
+            this.ctx.translate(42, 7);
+
+            this.ctx.beginPath();
+            this.ctx.moveTo(31.229,17.736);
+            this.ctx.bezierCurveTo(31.293,17.165,31.333,16.588,31.333,16);
+            this.ctx.bezierCurveTo(31.333,15.411999999999999,31.293,14.834,31.229,14.263);
+            this.ctx.lineTo(26.852,12.706);
+            this.ctx.bezierCurveTo(26.634,11.99,26.348,11.305,26.001,10.655999999999999);
+            this.ctx.lineTo(27.994,6.463999999999999);
+            this.ctx.bezierCurveTo(27.269,5.5539999999999985,26.445,4.729999999999999,25.536,4.004999999999999);
+            this.ctx.lineTo(21.343000000000004,5.998999999999999);
+            this.ctx.bezierCurveTo(20.696000000000005,5.651999999999999,20.009000000000004,5.366999999999999,19.294000000000004,5.149999999999999);
+            this.ctx.lineTo(17.736000000000004,0.7719999999999985);
+            this.ctx.bezierCurveTo(17.165,0.708,16.588,0.667,16,0.667);
+            this.ctx.bezierCurveTo(15.411999999999999,0.667,14.834,0.7080000000000001,14.263,0.772);
+            this.ctx.lineTo(12.707,5.15);
+            this.ctx.bezierCurveTo(11.991000000000001,5.367,11.306000000000001,5.652,10.657,5.9990000000000006);
+            this.ctx.lineTo(6.464,4.005);
+            this.ctx.bezierCurveTo(5.554,4.73,4.73,5.554,4.005,6.464);
+            this.ctx.lineTo(5.999,10.656);
+            this.ctx.bezierCurveTo(5.651999999999999,11.304,5.367,11.99,5.1499999999999995,12.706);
+            this.ctx.lineTo(0.7719999999999994,14.263);
+            this.ctx.bezierCurveTo(0.708,14.834,0.667,15.412,0.667,16);
+            this.ctx.bezierCurveTo(0.667,16.588,0.7080000000000001,17.165,0.772,17.736);
+            this.ctx.lineTo(5.15,19.294);
+            this.ctx.bezierCurveTo(5.367,20.009,5.652,20.695,5.9990000000000006,21.343);
+            this.ctx.lineTo(4.005000000000001,25.536);
+            this.ctx.bezierCurveTo(4.73,26.445,5.554,27.269000000000002,6.464,27.994);
+            this.ctx.lineTo(10.656,26.001);
+            this.ctx.bezierCurveTo(11.304,26.348000000000003,11.99,26.634,12.706,26.852);
+            this.ctx.lineTo(14.263,31.229);
+            this.ctx.bezierCurveTo(14.834,31.293,15.411,31.333,16,31.333);
+            this.ctx.bezierCurveTo(16.588,31.333,17.165,31.293,17.736,31.229);
+            this.ctx.lineTo(19.294,26.852);
+            this.ctx.bezierCurveTo(20.009,26.634,20.693,26.348,21.343,26.001);
+            this.ctx.lineTo(25.536,27.994);
+            this.ctx.bezierCurveTo(26.445,27.269,27.269000000000002,26.445,27.994,25.536);
+            this.ctx.lineTo(26.001,21.343000000000004);
+            this.ctx.bezierCurveTo(26.348000000000003,20.696000000000005,26.634,20.009000000000004,26.852,19.294000000000004);
+            this.ctx.lineTo(31.229,17.736);
+            this.ctx.closePath();
+            this.ctx.moveTo(16,20.871);
+            this.ctx.bezierCurveTo(13.31,20.871,11.128,18.689,11.128,15.999999999999998);
+            this.ctx.bezierCurveTo(11.128,13.309999999999999,13.31,11.127999999999998,16,11.127999999999998);
+            this.ctx.bezierCurveTo(18.689,11.127999999999998,20.871000000000002,13.309999999999999,20.871000000000002,15.999999999999998);
+            this.ctx.bezierCurveTo(20.871,18.689,18.689,20.871,16,20.871);
+            this.ctx.closePath();
+            this.ctx.fill();
+            this.ctx.stroke();
+            
+            this.ctx.restore();
+        });
+
+
         this.clear = function () {
             //this.ctx.moveTo(0, 0);
             this.ctx.clearRect(this.x, this.y, this.width, this.height);
@@ -45,10 +102,10 @@ define([
 
         this.inFigure = function (mouse) {
             var s = this.getScale();
-            if (this.x*s < mouse.x &&
-                this.x*s + this.width*s > mouse.x &&
-                this.y*s < mouse.y &&
-                this.y*s + this.height*s > mouse.y) {
+            if (this.x * s < mouse.x &&
+                this.x * s + this.width * s > mouse.x &&
+                this.y * s < mouse.y &&
+                this.y * s + this.height * s > mouse.y) {
                 return true;
             }
             return false;
@@ -56,7 +113,7 @@ define([
         this.onSelect = function (e) {
             var mouse = self.getPosition(e);
             //console.log(e, mouse, self.inFigure(mouse));
-            if(self.inFigure(mouse)) {
+            if (self.inFigure(mouse)) {
                 //console.log('OK');
 //                console.log(self.$scope);
                 self.$scope.hideDialog = false;
@@ -71,11 +128,11 @@ define([
              top: Math.ceil(2*self.getScale())+'px',
              'font-size': Math.ceil(26*self.getScale())+'px'
              });*/
-            $('.t4').css({
-                left: Math.ceil(47*self.getScale())+'px',
-                top: Math.ceil(2*self.getScale())+'px',
-                'font-size': Math.ceil(26*self.getScale())+'px'
-            }).on('click', this.onSelect);
+//            $('.t4').css({
+//                left: Math.ceil(47*self.getScale())+'px',
+//                top: Math.ceil(2*self.getScale())+'px',
+//                'font-size': Math.ceil(26*self.getScale())+'px'
+//            }).on('click', this.onSelect);
         }
     }
 
