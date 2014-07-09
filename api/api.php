1b08e24998619b66723c0e39e8115b3923332c55
Alex Slubsky <aslubsky@gmail.com>
2014-07-09 13:18:07 +0300

    refactoring

--- api/api.php
+++ api/api.php
@@ -2,11 +2,9 @@
 
 require_once __DIR__ . '/db.php';
 
-//echo file_get_contents(__DIR__.'/config.json');exit;
 $config = json_decode(file_get_contents(__DIR__ . '/config.json'));
-//print_r($config);exit;
-$connStr = sprintf('mysql:dbname=%s;host=%s;port=%s', $config->mysql->database, $config->mysql->host, $config->mysql->port);
-//echo $connStr;exit;
+
+/*$connStr = sprintf('mysql:dbname=%s;host=%s;port=%s', $config->mysql->database, $config->mysql->host, $config->mysql->port);
 $db = new DB($connStr, $config->mysql->user, $config->mysql->password);
 
 $res = array();
@@ -26,9 +24,35 @@ foreach ($config->dataSources as $dsCfg) {
 
 //function getColor($dsCfg, $value)
 
+echo $_GET['callback'] . '(' . json_encode($res) . ')';*/
+
+
+$memcache = new Memcached();
+
+if ($memcache->addServer('localhost', 11211) === false || @$memcache->getStats() === false) {
+    throw new Exception('Could not connect to Memcache server ');
+}
+
+$res = array();
+foreach ($config->dataSources as $dsCfg) {
+    $lastVal = array(
+        'date' => date('Y-m-d H:i:s'),
+        'source_id' => $dsCfg->id,
+//        'value' => (mt_rand(1200, 1300)/10).""
+        'value' => '0'
+    );
+
+    $date = $memcache->get('ds_d_'.$dsCfg->id);
+    $value = $memcache->get('ds_v_'.$dsCfg->id);
+    if($value !== false) {
+        $lastVa['date'] = $date;
+        $lastVa['value'] = $value;
+    }
+
+    $lastVal['name'] = $dsCfg->name;
+    $lastVal['color'] = '#D9D4D3';
+    $res [] = $lastVal;
+}
+
+
 echo $_GET['callback'] . '(' . json_encode($res) . ')';
-//print_r($db->fetchAll('SELECT `date`, source_id, value FROM archive_numeric ORDER BY `date` DESC LIMIT 1'));
-
-//echo $_GET['callback'].'({"A": 222})';
-
-//self.db.query('SELECT `date`, source_id, value FROM archive_numeric WHERE source_id = ? ORDER BY `date` DESC LIMIT 1', [ds.id], function (rows, fields) {
